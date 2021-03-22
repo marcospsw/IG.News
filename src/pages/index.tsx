199930f9e9a1c8cf505ca2603a6d47843a0eca0a
marcospsw <marcospsw96@gmail.com>
2021-03-22 16:37:26 -0300

    Feat: Finalizando Primeira Parte Aplicação SSG SSR

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+/* Chamadas da API */
+
+// Client-Side = Não preciso de SEO ou Infos que não precisam estar carregadas assim que a pagina aparece
+// Server-Side = Precisa-se de SEO porém dados dinamicos
+// Static Site Generation = Paginas que não mudam e podem ser compartilhadas com outros usuários
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>IG.News</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Codding" />
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IXswPBDxid7h6v2oNUAIge6');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 Hours
+  };
+};
